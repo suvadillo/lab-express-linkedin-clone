62f063f7b41b332ead77e57fa95ccd14c606281b
Susana <susana_vadillo@yahoo.es>
2018-02-04 15:28:11 +0100

    first iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,33 +1,56 @@
-var express      = require('express');
-var path         = require('path');
-var favicon      = require('serve-favicon');
-var logger       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
+var express = require("express");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
+const authRoutes = require("./routes/auth-routes");
+const siteRoutes = require('./routes/site-routes');
+const app = express();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// Mongoose configuration
+mongoose.connect("mongodb://localhost/linkedin-ironhack");
 
-var app = express();
+app.use(
+  session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+  })
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', 'layouts/main-layout');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', index);
-app.use('/users', users);
+// Routes
+app.use("/", authRoutes);
+app.use('/', siteRoutes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -36,11 +59,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
